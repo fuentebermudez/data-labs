9e1507f03c10a199cc58f509bd2e8ddbd3da0e1c
fuentebermudez <fuentebermudez@gmail.com>
2019-05-20 17:41:15 +0200

    Bonus completado

---
+++ module-1/lab-mysql/your-code/SQL/delete.sql
@@ -0,0 +1,5 @@
+--En mi seeding no está este registro porque al establecer VIN como
+  clave primario no me ha permitido meter duplicados por lo que lo he
+  tenido que modificar.
+
+delete from car where VIN="DAM41UDN3CHU2WVF6" and ID=4;
---
+++ module-1/lab-mysql/your-code/SQL/update.sql
@@ -0,0 +1,8 @@
+--Actualización campo store mal escrito
+update salesperson set store="Miami" where Name="Paige Turner";
+
+--Actualizacion emails customers
+update customer set Email="ppicasso@gmail.com" where name="Pablo Picasso";
+update customer set Email="lincoln@us.gov" where name="Abraham Lincoln";
+update customer set Email="hello@napoleon.me" where name="Napoleón Bonaparte";
+
