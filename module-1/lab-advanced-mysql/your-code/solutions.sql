6a93853ecd88854269841c001f20585f2212c8d7
Miguel Angel <fuentebermudez@gmail.com>
2019-05-29 12:36:09 +0200

    Completado Challenge-2

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,10 +1,14 @@
 ----------------------------------------------------------------------
+--CHALLENGE 1
+----------------------------------------------------------------------
+----------------------------------------------------------------------
 --STEP 1
 ----------------------------------------------------------------------
+create temporary table Royalties
 SELECT
-	ta.title_id,
-    ta.au_id,
-    t.price*s.qty*(t.royalty/100)*(ta.royaltyper/100)
+	ta.title_id title_id,
+    	ta.au_id au_id,
+    	t.price*s.qty*(t.royalty/100)*(ta.royaltyper/100) Profit
     
 FROM 
 	sales s
@@ -19,30 +23,87 @@ FROM
     join 
 	authors a 
     on ta.au_id=a.au_id
+;
+----------------------------------------------------------------------
+--STEP 2 
+----------------------------------------------------------------------
 
-----------------------------------------------------------------------
---STEP 
-----------------------------------------------------------------------
-create temporary table ProffitByTitle
+create temporary table ProffitByAuthor
 SELECT
-    ta.title_id title_id,
-    ta.au_id au_id,
-    t.price*s.qty*(t.royalty/100)*(ta.royaltyper/100) ProfitTitle
-    
-FROM 
-	sales s
-    JOIN
+	title_id,
+	au_id,
+	sum(Profit) Profit
+	
+FROM
+	Royalties
+GROUP BY
+        au_id,title_id;
+
+----------------------------------------------------------------------
+--STEP 3 
+----------------------------------------------------------------------
+
+SELECT
+	p.title_id title_id,
+	p.au_id au_id,
+	Profit + t.advance Profit
+FROM
+	ProffitByAuthor p
+JOIN
 	titles t
-    	on
-	s.title_id=t.title_id
-    JOIN
-	titleauthor ta
-    	on
-	ta.title_id=t.title_id
-    join 
-	authors a 
-    on ta.au_id=a.au_id
+	ON
+	t.title_id=p.title_id
+order by profit desc
+LIMIT 3
+;
+
+----------------------------------------------------------------------
+--CHALLENGE 2
+----------------------------------------------------------------------
 
 
-select  from ProffitByTitle;
+SELECT
+	profitByAuthor.title_id title_id,
+	profitByAuthor.au_id au_id,
+	Profit + t.advance Profit
+FROM
+	(SELECT
+		title_id,
+		au_id,
+		sum(royalties.Profit) Profit
+	
+	FROM
+		(SELECT
+			ta.title_id title_id,
+    			ta.au_id au_id,
+    			t.price*s.qty*(t.royalty/100)*(ta.royaltyper/100) Profit
+    
+		FROM 
+			sales s
+    		JOIN
+			titles t
+    		on
+			s.title_id=t.title_id
+    		JOIN
+			titleauthor ta
+    			on
+			ta.title_id=t.title_id
+    		join 
+			authors a 
+    			on
+			ta.au_id=a.au_id
+		)royalties
+
+	GROUP BY
+                au_id,title_id
+	)profitByAuthor
+
+JOIN
+	titles t
+	ON
+	t.title_id=profitByAuthor.title_id
+order by profit desc
+LIMIT 3
+;
+
 
