7254372e30b143d76c4782f46cbcb7b0106ce7f3
Miguel Angel de la Fuente <mdelafuente@boslan.com>
2019-10-08 19:27:59 +0200

    Cross validation

--- module-3/supervised-learning-project/your-code/source/Modelado.py
+++ module-3/supervised-learning-project/your-code/source/Modelado.py
@@ -6,12 +6,14 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import VotingRegressor
+from sklearn.model_selection import GridSearchCV
 
 import Transformer as T
 import Extract as e
 import Submission as s
 
 def train_and_evaluate(clf, X_train, y_train):
+
     clf.fit(X_train, y_train)
 
     print("Coefficient of determination on training set:", clf.score(X_train, y_train))
@@ -29,8 +31,20 @@ X_train, X_test, Y_train,Y_test=train_test_split(X,y,test_size=0.33,random_state
 
 
 #clf=ExtraTreesRegressor(n_estimators=5, max_features=3,random_state=0)
-clf = RandomForestRegressor(max_depth=3, random_state=0,n_estimators=100)
+#clf = RandomForestRegressor(max_depth=3, random_state=0,n_estimators=100)
 #clf = LinearRegression(normalize=True)
+
+param_grid = {
+    'bootstrap': [True],
+    'max_depth': [80, 90, 100, 110],
+    'max_features': [2, 3,4],
+    'min_samples_leaf': [3, 4, 5],
+    'min_samples_split': [8, 10, 12],
+    'n_estimators': [100, 200, 300, 1000]
+}# Create a based model
+clf = RandomForestRegressor()# Instantiate the grid search model
+clf = GridSearchCV(estimator = clf, param_grid = param_grid,
+                          cv = 3, n_jobs = -1, verbose = 2)
 
 clf=train_and_evaluate(clf,X_train, Y_train)
 
